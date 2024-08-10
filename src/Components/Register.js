5b6bdd977b6919e6840e596b77e2f389298e2250
susa001 <sivajaya3000@gmail.com>
2024-08-10 13:00:42 +0530

    RegisterPage_updated

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import RegisterImg from "../Assets/registerpage.jpg"
 
 
@@ -7,10 +6,16 @@ const Register = () => {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <div className="w-50 -mr-12"> 
-          <img src={RegisterImg} className=""/>
+       <div className="w-1/2 flex flex-col items-center"> 
+          <img src={RegisterImg} />
+          <div className="text-center ">
+            <p className="text-2xl font-bold">Flex Your Connections</p>
+            <p className="w-3/4 text-sm mt-2 mx-auto text-gray-dark">
+              Elevate your chats, build real bonds, and stay linked with your squad.
+            </p>
+          </div>
         </div>
-        <div className="space-y-3 -ml-8">
+        <div className="space-y-3 -ml-10">
           <div className="text-3xl font-bold text-center" >Create an Account</div>
           <div className="">
             <label className="block text-xl pb-2">
@@ -51,6 +56,16 @@ const Register = () => {
               type="password"
               className="bg-gray-light focus:outline-none p-2 w-full pr-[150px] rounded-md" 
             />
+          </div>
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="privacy-policy"
+              className="mr-2"
+            />
+            <label htmlFor="privacy-policy" className="text-sm">
+              I accept the <a href="/privacy-policy" className="text-blue-600 underline font-semibold">privacy policy</a>
+            </label>
           </div>
           <div className="pt-5 flex justify-center" >
             <button className="bg-[#A1E7FF] pb-2 pt-2 pr-5 pl-5 text-xl font-bold rounded-lg">
