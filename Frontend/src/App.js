c217fb12680e4493ee52d35af57366db8cef9304
sarathy <sarathykuppusamy0205@gmail.com>
2024-08-13 21:11:34 +0530

    Registrationsuccessfull page updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
+import Registerationsuccessful from "./Components/Registrationsuccessful";
 import { Route,Routes } from "react-router-dom";
 
 
@@ -13,6 +14,8 @@ function App()
         <Route path="/" element={<Login/>}/>
         <Route path="Home" element={<Home/>}/>
         <Route path="Register" element={<Register/>}/>
+        <Route path="Registrationsuccessful" element={<Registerationsuccessful/>}/>
+
       </Routes>
     </div>
   );
---
+++ Frontend/src/Components/Registrationsuccessful.js
@@ -0,0 +1,20 @@
+import React from "react";
+function Registrationsuccessful()
+{
+    return(
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <div className="green-500 text-5xl">
+          &#10003;
+        </div>
+        <div className="text-xl font-bold">
+          Successfully Registered
+        </div>
+      </div>
+    </div>
+    );
+
+        
+    
+}
+export default Registrationsuccessful;
