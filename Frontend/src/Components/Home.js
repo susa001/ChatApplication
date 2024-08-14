0613f71e742b931994657305c33d79022d3c0272
santhosh70103 <santhosh70103@gmail.com>
2024-08-14 22:24:25 +0530

    Update Home.js

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -3,42 +3,52 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div className="w-1/4 h-screen flex flex-col ">
-        <div className="">
-          <div>My Name</div>
-          <input type="text" placeholder="Search contacts" className="w-full" />
+      <div className="flex">
+        <div className="w-[25%] h-screen flex flex-col ">
+          <div className="">
+            <div>My Name</div>
+            <input
+              type="text"
+              placeholder="Search contacts"
+              className="w-full"
+            />
+          </div>
+          <div className="grid overflow-y-scroll scrollbarHide">
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+            <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+          </div>
         </div>
-        <div className="grid overflow-y-scroll scrollbarHide">
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
-          <span className="p-2 bg-blue border border-b-gray-dark">nam1</span>
+
+        <div className=" w-[75%]">
+          
         </div>
       </div>
     </>
